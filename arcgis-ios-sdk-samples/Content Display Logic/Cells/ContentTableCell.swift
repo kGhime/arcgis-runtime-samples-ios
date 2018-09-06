e554cf9e21ca632bf589711bb2872497a7b2ad46
Nicholas Furness <nfurness@nixta.com>
2018-09-05 20:31:44 -0400

    Hide empty cells

--- arcgis-ios-sdk-samples/Content Display Logic/Cells/ContentTableCell.swift
+++ arcgis-ios-sdk-samples/Content Display Logic/Cells/ContentTableCell.swift
@@ -19,26 +19,21 @@ class ContentTableCell: UITableViewCell {
     @IBOutlet var titleLabel:UILabel!
     @IBOutlet var detailLabel:UILabel!
     @IBOutlet var infoButton:UIButton!
-    @IBOutlet var parentView:UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
-        parentView.layer.borderColor = UIColor(white: 0.85, alpha: 1).cgColor
-        parentView.layer.borderWidth = 1
     }
 
     override func setHighlighted(_ highlighted: Bool, animated: Bool) {
         
         if highlighted {
-            self.parentView.backgroundColor = .secondaryBlue
             self.titleLabel.textColor = .white
             self.detailLabel.textColor = .white
             self.infoButton.tintColor = .white
         }
         else {
-            self.parentView.backgroundColor = .white
             self.titleLabel.textColor = .primaryTextColor
             self.detailLabel.textColor = .secondaryTextColor
             self.infoButton.tintColor = .secondaryBlue
@@ -48,13 +43,11 @@ class ContentTableCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         
         if selected {
-            self.parentView.backgroundColor = .secondaryBlue
             self.titleLabel.textColor = .white
             self.detailLabel.textColor = .white
             self.infoButton.tintColor = .white
         }
         else {
-            self.parentView.backgroundColor = .white
             self.titleLabel.textColor = .primaryTextColor
             self.detailLabel.textColor = .secondaryTextColor
             self.infoButton.tintColor = .secondaryBlue
